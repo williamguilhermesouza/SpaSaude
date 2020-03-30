d2ca2e95f7ccfb7da520de2f79e8deeb21736218
williamguilhermesouza <williamguilhermesouza@gmail.com>
2020-03-29 21:26:37 -0300

    first app structuring

---
+++ mobile/App.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import Main from './src/pages/Main';
+import { Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <Main />
+  );
+}
---
+++ mobile/src/pages/Main/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+
+import styles from './styles';
+
+export default function Main() {
+    return (
+        <View style={styles.container}>
+            <Text>Hello Friend</Text>
+        </View>
+    );
+};
