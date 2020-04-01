ef7dfd0c06d6ec52ff7512a24838d497e812b637
william <williamguilhermesouza@gmail.com>
2020-03-31 21:08:35 -0300

    fixed loadfonts bug and removed splash assets

--- mobile/App.js
+++ mobile/App.js
@@ -1,13 +1,31 @@
-import React from 'react';
+import React, {useState, useEffect } from 'react';
 import Main from './src/pages/Main';
+import { AppLoading } from 'expo';
 import * as Font from 'expo-font';
 
-Font.loadAsync({
-  'IM Fell Double Pica': require('./src/assets/fonts/IMFellDoublePica-Regular.ttf'),
-  'ABeeZee': require('./src/assets/fonts/ABeeZee-Regular.ttf')
-});
+
 
 export default function App() {
+  let [fonts, setFonts] = useState(true);
+
+  async function loadFonts() {
+    await Font.loadAsync({
+      'IM Fell Double Pica': require('./src/assets/fonts/IMFellDoublePica-Regular.ttf'),
+      'ABeeZee': require('./src/assets/fonts/ABeeZee-Regular.ttf')
+    });
+    setFonts(false);
+  }
+
+  
+  if (fonts) {
+    return (
+      <AppLoading
+        startAsync={() => loadFonts()}
+        onFinish={() => {}}
+        onError={console.warn}
+      />
+    );
+  }
   return (
     <Main />
   );
