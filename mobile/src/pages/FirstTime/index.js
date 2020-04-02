a51318a4e462be762d42fe26092d625dcb3fabf0
william <williamguilhermesouza@gmail.com>
2020-04-02 11:01:32 -0300

    finished FirstTime page

--- mobile/src/pages/FirstTime/index.js
+++ mobile/src/pages/FirstTime/index.js
@@ -1,8 +1,112 @@
 import React from 'react';
-import { View } from 'react-native';
+import { View, Text, TextInput, TouchableOpacity } from 'react-native';
+import { useNavigation } from '@react-navigation/native';
+
 
 import styles from './styles';
 
 export default function FirstTime() {
-    return <View style={styles.container} />
+    const navigation = useNavigation();
+
+    function navigateToMain() {
+        navigation.navigate('Main');
+    };
+
+    return (
+    <View style={styles.container}>
+        <TextInput 
+            style={styles.input}
+            placeholder="Nome" 
+            onChangeText={() => {}} 
+        />
+
+        <View style={styles.doubleInput}>
+            <TextInput 
+                style={styles.doubleInputInput}
+                placeholder="Idade" 
+                onChangeText={() => {}} 
+            />
+            <TextInput 
+                style={styles.doubleInputInput}
+                placeholder="Sexo" 
+                onChangeText={() => {}} 
+            />
+        </View>
+        
+        <TextInput
+            style={styles.input} 
+            placeholder="CPF" 
+            onChangeText={() => {}} 
+        />
+        <TextInput
+            style={styles.input} 
+            placeholder="E-mail" 
+            onChangeText={() => {}} 
+        />
+        <TextInput
+            style={styles.input} 
+            placeholder="Telefone" 
+            onChangeText={() => {}} 
+        />
+        <TextInput 
+            style={styles.input} 
+            placeholder="Endereço" 
+            onChangeText={() => {}} 
+        />
+
+        <View style={styles.doubleInput}>
+            <TextInput
+                style={styles.doubleInputInput} 
+                placeholder="Número" 
+                onChangeText={() => {}} 
+            />
+            <TextInput
+                style={styles.doubleInputInput} 
+                placeholder="Bairro" 
+                onChangeText={() => {}} 
+            />
+        </View>
+
+        <View style={styles.doubleInput}>
+            <TextInput 
+                style={styles.doubleInputInput} 
+                placeholder="Cidade" 
+                onChangeText={() => {}} 
+            />
+            <TextInput 
+                style={styles.doubleInputInput} 
+                placeholder="UF" 
+                onChangeText={() => {}} 
+            />
+        </View>
+
+        <TextInput 
+            style={styles.input} 
+            placeholder="Complemento" 
+            onChangeText={() => {}} 
+        />
+        <TextInput 
+            style={styles.input} 
+            placeholder="Usuário" 
+            onChangeText={() => {}} 
+        />
+        <TextInput 
+            style={styles.input} 
+            placeholder="Senha" 
+            onChangeText={() => {}} 
+        />
+        <TextInput 
+            style={styles.input} 
+            placeholder="Confirme sua Senha" 
+            onChangeText={() => {}} 
+        />
+
+        <TouchableOpacity 
+            style={styles.avancarButton}
+            onPress={navigateToMain}
+        >
+            <Text style={styles.avancarText}>Avançar</Text>
+        </TouchableOpacity>
+    </View>
+    );
 };
--- mobile/src/pages/FirstTime/styles.js
+++ mobile/src/pages/FirstTime/styles.js
@@ -5,5 +5,46 @@ export default StyleSheet.create({
         flex: 1,
         flexDirection: 'column',
         alignItems: 'center',
+        marginLeft: 10,
+        marginRight: '10%',
     },
+
+    input: {
+        paddingTop: 5,
+        borderBottomColor: '#E5E5E5',
+        width: '80%',
+        borderBottomWidth: 3,
+        alignSelf: 'flex-start',
+    },
+
+    doubleInput: {
+        width: '100%',
+        flexDirection: 'row',
+        justifyContent: 'space-between'
+        
+    },
+    
+    doubleInputInput: {
+        paddingTop: 5,
+        borderBottomColor: '#E5E5E5',
+        borderBottomWidth: 3,
+        width: '50%',
+        marginRight: 20,
+    },
+
+    avancarButton: {
+        marginTop: 60,
+        alignItems: 'center',
+        marginLeft: 35,
+        width: '60%',
+        height: '9%',
+        backgroundColor: '#F4AA8A',
+        borderRadius: 10,
+    },
+
+    avancarText: {
+        paddingTop: 10,
+        fontSize: 26,
+        fontFamily: 'ABeeZee',
+    }
 });
