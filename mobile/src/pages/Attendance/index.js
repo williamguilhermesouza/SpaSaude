ce1faa6794b136302d5bc6401838d39f4145e780
william <williamguilhermesouza@gmail.com>
2020-04-19 21:25:03 -0300

    working on Attendance screen

--- mobile/src/pages/Attendance/index.js
+++ mobile/src/pages/Attendance/index.js
@@ -1,20 +1,48 @@
 import React, { useState } from 'react';
-import { View, Text, TouchableOpacity } from 'react-native';
+import { View, Text, TouchableOpacity, FlatList } from 'react-native';
 
 
 import styles from './styles';
 
 export default function Attendance() {
+    [ expand, setExpand ] = useState(false);
 
     function navigateToSchedule() {};
-    
+
     return (
         <View style={styles.container}>
             <TouchableOpacity
-                style={styles.massageOpacity}
+                style={styles.massageExpandOpacity}
                 onPress={() => {}}    
             >
                 <Text style={styles.massageOpacityTitle}>Massagem Completa</Text>
+
+                <View style={styles.massageList}>
+                    <Text style={styles.massageListTitle}>Pedras Quentes</Text>
+                    <Text>&#9679; Aumento da circulação sanguínea;</Text>
+                    <Text>&#9679; Relaxamento profundo;</Text>
+                    <Text>&#9679; Aumento da drenagem linfática;</Text>
+                    <Text>&#9679; Alívio das dores musculares;</Text>
+                    <Text>&#9679; Diminuição do estresse e tensões;</Text>
+                    <Text>&#9679; Aumento do bem estar;</Text>
+                </View>
+
+                <View style={styles.massageListFooter}>
+                    <TouchableOpacity>
+                        <Text style={styles.opsText}> - </Text>
+                    </TouchableOpacity>
+
+                    <Text style={styles.opsText}> 0 </Text>
+
+                    <TouchableOpacity>
+                        <Text style={styles.opsText}> + </Text>
+                    </TouchableOpacity>
+
+                    <TouchableOpacity style={styles.addButton}>
+                        <Text style={styles.addButtonText}> Adicionar R$ 150,00 </Text>
+                    </TouchableOpacity>
+                </View> 
+
             </TouchableOpacity>
 
             <TouchableOpacity 
--- mobile/src/pages/Attendance/styles.js
+++ mobile/src/pages/Attendance/styles.js
@@ -8,6 +8,74 @@ export default StyleSheet.create({
         alignItems: 'center',
     },
 
+    massageCollapseOpacity: {
+        alignItems: 'center',
+        marginBottom: 45,
+        width: '80%',
+        height: '9%',
+        backgroundColor: '#FDD4C3',
+        borderRadius: 15,
+    },
+
+    massageExpandOpacity: {
+        alignItems: 'center',
+        marginBottom: 45,
+        width: '80%',
+        height: '60%',
+        backgroundColor: '#FDD4C3',
+        borderRadius: 15,
+    },
+
+    massageOpacityTitle: {
+        paddingTop: 10,
+        fontSize: 20,
+        fontStyle: 'italic',
+        fontFamily: 'ABeeZee',
+    },
+
+    massageList: {
+        alignItems: 'flex-start',
+        width: '86%',
+        height: '65%',
+    },
+
+    massageListTitle: {
+        width: '100%',
+        fontSize: 18,
+        fontWeight: 'bold',
+        borderBottomWidth: 1,
+        marginTop: 10,
+        paddingBottom: 10,
+        marginBottom: 5,
+    },
+
+    massageListFooter: {
+        flexDirection: 'row',
+        marginTop: 20,
+        width: '85%',
+
+    },
+
+    opsText: {
+        fontWeight: 'bold',
+        fontSize: 22,
+    },
+
+    addButton: {
+        marginLeft: 20,
+        alignItems: 'center',
+        width: '70%',
+        backgroundColor: '#F4AA8A',
+        borderRadius: 5,
+    },
+
+    addButtonText: {
+        paddingTop: 5,
+        fontFamily: 'ABeeZee',
+        fontSize: 18,
+        paddingBottom: 5,
+    },
+
     nextButton: {
         alignItems: 'center',
         width: '60%',
@@ -19,22 +87,6 @@ export default StyleSheet.create({
     nextText: {
         paddingTop: 10,
         fontSize: 26,
-        fontFamily: 'ABeeZee',
-    },
-
-    massageOpacity: {
-        alignItems: 'center',
-        marginBottom: 45,
-        width: '80%',
-        height: '9%',
-        backgroundColor: '#FDD4C3',
-        borderRadius: 15,
-    },
-
-    massageOpacityTitle: {
-        paddingTop: 10,
-        fontSize: 20,
-        fontStyle: 'italic',
         fontFamily: 'ABeeZee',
     },
 });
