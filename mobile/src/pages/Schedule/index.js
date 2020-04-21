5441853f7963199513029cff79b56090601d9cdc
william <williamguilhermesouza@gmail.com>
2020-04-21 19:29:57 -0300

    finished schedule screen layout

--- mobile/src/pages/Schedule/index.js
+++ mobile/src/pages/Schedule/index.js
@@ -1,12 +1,77 @@
-import React from 'react';
-import { View, Text } from 'react-native';
+import React, { useState } from 'react';
+import { View, Text, TouchableOpacity, Platform } from 'react-native';
+import { useNavigation } from '@react-navigation/native';
+import DateTimePicker from '@react-native-community/datetimepicker';
+import SegmentedControl from '@react-native-community/segmented-control';
+
 
 import styles from './styles';
 
 export default function Schedule() {
+    const navigation = useNavigation();
+    let [payment, setPayment] = useState('Dinheiro');
+    let [date, setDate] = useState(Date.now());
+    let [mode, setMode] = useState('date');
+    let [show, setShow] = useState(false);
+
+    function calendarChangeHandler(event, selectedDate) {
+        const currentDate = selectedDate || date;
+        setShow(Platform.OS === 'ios');
+        setDate(currentDate);
+    };
+
+    function showCalendar(currentMode) {
+        setShow(true);
+        setMode(currentMode);
+    };
+
+    function navigateToConfirmation() {
+        navigation.navigate('Confirmation');
+    };
+
     return (
         <View style={styles.container}>
-            <Text> Hello_Friend </Text>
+
+            <TouchableOpacity
+                style={styles.pickerButton}
+                onPress={() => showCalendar('date')}
+            >
+                <Text style={styles.pickerButtonText}>Data</Text>
+            </TouchableOpacity>
+
+            <TouchableOpacity
+                style={styles.pickerButton}
+                onPress={() => showCalendar('time')}
+            >
+                <Text style={styles.pickerButtonText}>Horários Disponíveis</Text>
+            </TouchableOpacity>
+
+            {show &&
+                <DateTimePicker
+                    value={date}
+                    mode={mode}
+                    is24Hour={true}
+                    display='spinner'
+                    onChange={calendarChangeHandler}
+                />
+            }
+
+            <View style={styles.paymentSelector}>
+                <Text style={styles.paymentLabel}>Forma de Pagamento</Text>
+                <SegmentedControl  
+                    selectedIndex={payment}
+                    backgroundColor='pink'
+                    values={['Dinheiro', 'Débito', 'Crédito']}
+                    onChange={ (event) => { setPayment(event.nativeEvent.selectedSegmentIndex); }}
+                />
+            </View>
+
+            <TouchableOpacity 
+                    style={styles.nextButton}
+                    onPress={navigateToConfirmation}
+                >
+                    <Text style={styles.nextText}>Avançar</Text>
+                </TouchableOpacity>
         </View>
     );
 };
--- mobile/src/pages/Schedule/styles.js
+++ mobile/src/pages/Schedule/styles.js
@@ -2,6 +2,51 @@ import { StyleSheet } from 'react-native';
 
 export default StyleSheet.create({
     container: {
-        flex: 1
+        flex: 1,
+        flexDirection: 'column',
+        alignItems: 'center',
+    },
+
+    pickerButton: {
+        marginTop: '20%',
+        width: '60%',
+        borderBottomWidth: 1,
+        alignItems: 'center',
+    },
+
+    pickerButtonText: {
+        fontFamily: 'ABeeZee',
+        fontSize: 18,
+    },
+
+    paymentLabel: {
+        alignSelf: 'center',
+        fontFamily: 'ABeeZee',
+        fontSize: 18,
+        marginBottom: 20,
+    },
+
+    paymentSelector: {
+        width: '70%',
+        marginTop: '20%',
+        marginBottom: '17%',
+    },
+
+    nextButton: {
+        bottom: 50,
+        marginTop: 70,
+        alignSelf: 'center',
+        alignItems: 'center',
+        width: '60%',
+        height: 50,
+        backgroundColor: '#F4AA8A',
+        borderRadius: 10,
+
+    },
+
+    nextText: {
+        paddingTop: 10,
+        fontSize: 26,
+        fontFamily: 'ABeeZee',
     },
 });
