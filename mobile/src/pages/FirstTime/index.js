a43e0588bd02f98ebece548653a5fc53bd8052c1
williamguilhermesouza <williamguilhermesouza@gmail.com>
2020-07-04 14:25:32 -0300

    linked firsttime and login to realtime and auth

--- mobile/src/pages/FirstTime/index.js
+++ mobile/src/pages/FirstTime/index.js
@@ -1,112 +1,161 @@
-import React from 'react';
-import { View, Text, TextInput, TouchableOpacity } from 'react-native';
+import React, { useState } from 'react';
+import { View, Text, TextInput, TouchableOpacity, KeyboardAvoidingView, Alert } from 'react-native';
 import { useNavigation } from '@react-navigation/native';
 
+import firebase from 'firebase';
 
 import styles from './styles';
 
 export default function FirstTime() {
     const navigation = useNavigation();
 
-    function navigateToLogin() {
-        navigation.navigate('Login');
+    let [name, setName] = useState('');
+    let [age, setAge] = useState(0);
+    let [sex, setSex] = useState('');
+    let [cpf, setCpf] = useState('');
+    let [email, setEmail] = useState('');
+    let [phone, setPhone] = useState('');
+    let [address, setAddress] = useState('');
+    let [anumber, setANumber] = useState(0);
+    let [anbh, setAnbh] = useState('');
+    let [acity, setACity] = useState('');
+    let [astate, setAState] = useState('');
+    let [acomp, setAComp] = useState('');
+    let [pass, setPass] = useState('');
+    let [passConf, setPassConf] = useState('');
+
+
+    async function navigateToLogin() {
+        let auth = await firebase.auth().createUserWithEmailAndPassword(email, pass).catch( (e) => {
+           Alert.alert(
+               "Erro ao criar",
+               e.message,
+           );
+
+        });
+
+        if (pass != passConf) {
+            Alert.alert("Erro", "Senha e Confirme sua senha devem ser iguais");
+            return;
+        }
+
+        if ( typeof auth != 'undefined') {
+            firebase.database().ref('users/' + name).set({
+                'name': name,
+                'age': age,
+                'sex': sex,
+                'cpf': cpf,
+                'email': email,
+                'phone': phone,
+                'address': address,
+                'anumber': anumber,
+                'anbh': anbh,
+                'acity': acity,
+                'astate': astate,
+                'acomp': acomp,
+            }).catch( e => {
+                Alert.alert(
+                    "Valor inválido",
+                    e.message,
+                );
+            });
+
+            navigation.navigate('Login');
+        }
     };
 
     return (
-    <View style={styles.container}>
-        <TextInput 
-            style={styles.input}
-            placeholder="Nome" 
-            onChangeText={() => {}} 
-        />
-
-        <View style={styles.doubleInput}>
+        <KeyboardAvoidingView style={styles.container} behavior='padding'>
             <TextInput 
-                style={styles.doubleInputInput}
-                placeholder="Idade" 
-                onChangeText={() => {}} 
+                style={styles.input}
+                placeholder="Nome" 
+                onChangeText={text => setName(text)} 
             />
-            <TextInput 
-                style={styles.doubleInputInput}
-                placeholder="Sexo" 
-                onChangeText={() => {}} 
-            />
-        </View>
-        
-        <TextInput
-            style={styles.input} 
-            placeholder="CPF" 
-            onChangeText={() => {}} 
-        />
-        <TextInput
-            style={styles.input} 
-            placeholder="E-mail" 
-            onChangeText={() => {}} 
-        />
-        <TextInput
-            style={styles.input} 
-            placeholder="Telefone" 
-            onChangeText={() => {}} 
-        />
-        <TextInput 
-            style={styles.input} 
-            placeholder="Endereço" 
-            onChangeText={() => {}} 
-        />
 
-        <View style={styles.doubleInput}>
+            <View style={styles.doubleInput}>
+                <TextInput 
+                    style={styles.doubleInputInput}
+                    placeholder="Idade" 
+                    onChangeText={text => setAge(text)} 
+                />
+                <TextInput 
+                    style={styles.doubleInputInput}
+                    placeholder="Sexo" 
+                    onChangeText={text => setSex(text)}
+                />
+            </View>
+            
             <TextInput
-                style={styles.doubleInputInput} 
-                placeholder="Número" 
-                onChangeText={() => {}} 
+                style={styles.input} 
+                placeholder="CPF" 
+                onChangeText={text => setCpf(text)}
             />
             <TextInput
-                style={styles.doubleInputInput} 
-                placeholder="Bairro" 
-                onChangeText={() => {}} 
+                style={styles.input} 
+                placeholder="E-mail" 
+                onChangeText={text => setEmail(text)} 
             />
-        </View>
-
-        <View style={styles.doubleInput}>
-            <TextInput 
-                style={styles.doubleInputInput} 
-                placeholder="Cidade" 
-                onChangeText={() => {}} 
+            <TextInput
+                style={styles.input} 
+                placeholder="Telefone" 
+                onChangeText={text => setPhone(text)}
             />
             <TextInput 
-                style={styles.doubleInputInput} 
-                placeholder="UF" 
-                onChangeText={() => {}} 
+                style={styles.input} 
+                placeholder="Endereço" 
+                onChangeText={text => setAddress(text)}
             />
-        </View>
 
-        <TextInput 
-            style={styles.input} 
-            placeholder="Complemento" 
-            onChangeText={() => {}} 
-        />
-        <TextInput 
-            style={styles.input} 
-            placeholder="Usuário" 
-            onChangeText={() => {}} 
-        />
-        <TextInput 
-            style={styles.input} 
-            placeholder="Senha" 
-            onChangeText={() => {}} 
-        />
-        <TextInput 
-            style={styles.input} 
-            placeholder="Confirme sua Senha" 
-            onChangeText={() => {}} 
-        />
+            <View style={styles.doubleInput}>
+                <TextInput
+                    style={styles.doubleInputInput} 
+                    placeholder="Número" 
+                    onChangeText={text => setANumber(text)}
+                />
+                <TextInput
+                    style={styles.doubleInputInput} 
+                    placeholder="Bairro" 
+                    onChangeText={text => setAnbh(text)}
+                />
+            </View>
 
-        <TouchableOpacity 
-            style={styles.avancarButton}
-            onPress={navigateToLogin}
-        >
-            <Text style={styles.avancarText}>Avançar</Text>
-        </TouchableOpacity>
-    </View>
+            <View style={styles.doubleInput}>
+                <TextInput 
+                    style={styles.doubleInputInput} 
+                    placeholder="Cidade" 
+                    onChangeText={text => setACity(text)} 
+                />
+                <TextInput 
+                    style={styles.doubleInputInput} 
+                    placeholder="UF" 
+                    onChangeText={text => setAState(text)} 
+                />
+            </View>
+
+            <TextInput 
+                style={styles.input} 
+                placeholder="Complemento" 
+                onChangeText={text => setAComp(text)} 
+            />
+            <TextInput 
+                style={styles.input} 
+                secureTextEntry={true}
+                placeholder="Senha" 
+                onChangeText={text => setPassConf(text)} 
+            />
+            <TextInput 
+                style={styles.input} 
+                secureTextEntry={true}
+                placeholder="Confirme sua Senha" 
+                onChangeText={text => { text == passConf ? setPass(text) : console.log("Different") }} 
+            />
+
+            <TouchableOpacity 
+                style={styles.avancarButton}
+                onPress={navigateToLogin}
+            >
+                <Text style={styles.avancarText}>Avançar</Text>
+            </TouchableOpacity>
+        </KeyboardAvoidingView>
     );
 };
--- mobile/src/pages/Login/index.js
+++ mobile/src/pages/Login/index.js
@@ -1,6 +1,7 @@
-import React from 'react';
-import { View, Text, TouchableOpacity, Image, TextInput } from 'react-native';
+import React, { useState } from 'react';
+import { View, Text, TouchableOpacity, Image, TextInput, Alert } from 'react-native';
 import { useNavigation } from '@react-navigation/native';
+import firebase from 'firebase';
 
 import styles from './styles';
 
@@ -9,12 +10,20 @@ import titleImg from '../../assets/main.jpg';
 // fix keyboard gets in front of input
 export default function Login() {
     const navigation = useNavigation();
+    let [email, setEmail] = useState('');
+    let [password, setPassword] = useState('');
 
     function navigateTofPass() {
         navigation.navigate('ForgotPassword');
     };
-    function navigateToAttendance() {
-        navigation.navigate('Attendance');
+    async function navigateToAttendance() {
+        let auth = await firebase.auth().signInWithEmailAndPassword(email, password).catch( e => {
+            Alert.alert("Login Incorreto", e.message);
+        });
+        
+        if (typeof auth != 'undefined') {
+            navigation.navigate('Attendance');
+        }
     };
 
     return (
@@ -26,15 +35,16 @@ export default function Login() {
             </View>
             <TextInput 
                 style={styles.input}
-                placeholder="Login" 
+                placeholder="E-mail" 
                 placeholderTextColor='#777'
-                onChangeText={() => {}} 
+                onChangeText={text => setEmail(text)} 
             />
             <TextInput 
                 style={{ ...styles.input, marginTop: 20 }}
                 placeholder="Senha" 
                 placeholderTextColor='#777'
-                onChangeText={() => {}} 
+                secureTextEntry={true}
+                onChangeText={text => setPassword(text)} 
             />
 
             <TouchableOpacity 
